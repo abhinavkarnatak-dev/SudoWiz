4648261cf2083bc0eb7afca2bae97ef58e775425
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2024-07-27 11:23:02 +0530

    Hover functionality updated

--- src/components/SudokuGrid.jsx
+++ src/components/SudokuGrid.jsx
@@ -4,7 +4,8 @@ import solveSudoku from '../utils/backtracking';
 const SudokuGrid = ({ initialGrid }) => {
   const [grid, setGrid] = useState(Array(9).fill().map(() => Array(9).fill('')));
   const [solvedCells, setSolvedCells] = useState([]);
-  const [focusedCell, setFocusedCell] = useState({ row: 0, col: 0 });
+  const [focusedCell, setFocusedCell] = useState(null);
+  const [isSolved, setIsSolved] = useState(false);
   const inputRefs = useRef(Array(9).fill().map(() => Array(9).fill(null)));
 
   useEffect(() => {
@@ -31,6 +32,8 @@ const SudokuGrid = ({ initialGrid }) => {
     if (isSolved) {
       setGrid(gridCopy);
       setSolvedCells(solvedCellsList);
+      setIsSolved(true);
+      setFocusedCell(null);
     } else {
       alert('No solution exists!');
     }
@@ -39,10 +42,13 @@ const SudokuGrid = ({ initialGrid }) => {
   const handleReset = () => {
     setGrid(Array(9).fill().map(() => Array(9).fill('')));
     setSolvedCells([]);
-    setFocusedCell({ row: 0, col: 0 });
+    setFocusedCell(null);
+    setIsSolved(false);
   };
 
   const handleKeyDown = (e, rowIndex, colIndex) => {
+    if (isSolved) return;
+
     const { key } = e;
     const gridSize = 9;
 
@@ -68,6 +74,7 @@ const SudokuGrid = ({ initialGrid }) => {
   };
 
   const handleFocus = (rowIndex, colIndex) => {
+    if (isSolved) return;
     setFocusedCell({ row: rowIndex, col: colIndex });
   };
 
@@ -85,7 +92,7 @@ const SudokuGrid = ({ initialGrid }) => {
                 onKeyDown={(e) => handleKeyDown(e, rowIndex, colIndex)}
                 onFocus={() => handleFocus(rowIndex, colIndex)}
                 ref={(el) => inputRefs.current[rowIndex][colIndex] = el}
-                className={`w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-12 lg:h-12 text-center text-zinc-800 hover:bg-orange-300 hover:bg-opacity-100 text-2xl font-bold border
+                className={`w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-12 lg:h-12 text-center text-zinc-800 ${isSolved ? 'cursor-not-allowed' : 'hover:bg-orange-300 hover:bg-opacity-100'} text-2xl font-bold border
                             ${colIndex % 3 === 0 || colIndex === 0 ? 'border-l-4 border-gray-500' : 'border-l-1'}
                             ${rowIndex % 3 === 0 || rowIndex === 0 ? 'border-t-4 border-gray-500' : 'border-t-1'}
                             ${colIndex === 8 ? 'border-r-4 border-gray-500' : ''}
@@ -93,12 +100,13 @@ const SudokuGrid = ({ initialGrid }) => {
                             ${colIndex !== 8 ? 'border-r-0 border-gray-500' : ''}
                             ${rowIndex !== 8 ? 'border-b-0 border-gray-500' : ''}
                             ${solvedCells.some(([r, c]) => r === rowIndex && c === colIndex) ? 'bg-green-300' : ''}
-                            ${focusedCell.row === rowIndex && focusedCell.col === colIndex ? 'bg-orange-300' : ''}
+                            ${focusedCell?.row === rowIndex && focusedCell?.col === colIndex ? 'bg-orange-300' : ''}
                             `}
                 style={{ 
                   '-moz-appearance': 'textfield',
                   '-webkit-appearance': 'none',
-                  'appearance': 'none'
+                  'appearance': 'none',
+                  pointerEvents: isSolved ? 'none' : 'auto'
                 }}
               />
             ))
